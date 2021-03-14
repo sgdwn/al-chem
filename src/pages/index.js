1dea0567491b566fb47b359a2206ccc4d776cd4e
sgdwn <74820599+sgdwn@users.noreply.github.com>
2021-03-14 13:14:20 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,7 @@ function Home() {
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
           <div className={styles.buttons}>
           <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
           <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
