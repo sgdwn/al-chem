b61bd85ab4244ae06da1f0760b1e8681bffa76e8
sgdwn <74820599+sgdwn@users.noreply.github.com>
2021-03-14 16:46:05 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
+    Introduction: ['intro'],
     'Physical Chemistry': ['doc1', 'doc2', 'bonding', 'REDOX', 'equilibria', 'physical-revision'],
     'Inorganic Chemistry': ['periodicity', 'group2', 'group7'],
     'Organic Chemistry': ['mdx'],
